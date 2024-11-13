eaf8b69a89b009b7cc0b8563f5453cceac40cfe1
MichaelSitanggang <michaelsitanggang37@gmail.com>
2024-11-13 14:50:47 +0700

    di branch main

--- services/inputUsecase.go
+++ services/inputUsecase.go
@@ -8,7 +8,6 @@ import (
 type InputUseCase interface {
 	Findall(userID int) ([]entities.Input_aktivitas, error)
 	CreateAktip(userID int, akttivitas *entities.Input_aktivitas) error
-	Update(userID int, aktivitasID int, aktivitas *entities.Input_aktivitas) error
 }
 
 type inputUseCase struct {
@@ -27,16 +26,4 @@ func (iuc inputUseCase) CreateAktip(userID int, aktivitas *entities.Input_aktivi
 	aktivitas.Total_jejak_karbon = aktivitas.Data_Aktivitas * float64(aktivitas.Konsumsi_energi_kwh)
 	aktivitas.User_id = userID
 	return iuc.repo.CreateAktivitas(aktivitas)
-}
-
-func (iuc inputUseCase) Update(aktivitasID int, userID int, aktivitas *entities.Input_aktivitas) error {
-	activ, err := iuc.repo.FindByID(aktivitasID, userID)
-	if err != nil {
-		return err
-	}
-	activ.Data_Aktivitas = aktivitas.Data_Aktivitas
-	activ.Konsumsi_energi_kwh = aktivitas.Konsumsi_energi_kwh
-	activ.Total_jejak_karbon = aktivitas.Data_Aktivitas * float64(aktivitas.Konsumsi_energi_kwh)
-
-	return iuc.repo.UpdateAktivitas(activ)
 }
