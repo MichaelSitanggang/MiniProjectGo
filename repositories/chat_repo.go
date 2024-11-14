35da031ff335ef1171cb4aec67bebc2a8b95b317
MichaelSitanggang <michaelsitanggang37@gmail.com>
2024-11-14 17:35:43 +0700

    Masih buat repo

--- repositories/chat_repo.go
+++ repositories/chat_repo.go
@@ -1,7 +1,29 @@
 package repositories
 
-import "github.com/MichaelSitanggang/MiniProjectGo/entities"
+import (
+	"github.com/MichaelSitanggang/MiniProjectGo/entities"
+	"gorm.io/gorm"
+)
 
 type ChatRepo interface {
 	SaveChat(chat entities.Chat) error
+	GetAllChat() ([]entities.Chat, error)
+}
+
+type chatRepo struct {
+	db *gorm.DB
+}
+
+func NewChatRepo(db *gorm.DB) ChatRepo {
+	return &chatRepo{db: db}
+}
+
+func (r *chatRepo) SaveChat(chat entities.Chat) error {
+	return r.db.Create(&chat).Error
+}
+
+func (r *chatRepo) GetAllChat() ([]entities.Chat, error) {
+	var chats []entities.Chat
+	err := r.db.Find(chats).Error
+	return chats, err
 }
